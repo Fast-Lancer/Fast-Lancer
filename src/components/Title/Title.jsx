6af2ceeabf527797d700ff26910c9b03fb46c09e
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2022-01-19 09:02:30 -0800

    pageHeader added

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,11 +1,11 @@
 import styles from './Title.module.css'
 import TitleNav from './TitleNav'
 
-export default function Title({ pageTitle }) {
+export default function Title({ pageTitle, pageHeader }) {
   return (
     <div className={styles.titleBody}>
-      <section className={styles.title}>{pageTitle}</section>
-      <TitleNav pageTitle={pageTitle} />
+      <section className={styles.title}>{pageHeader}</section>
+      <TitleNav pageTitle={pageTitle, pageHeader} />
     </div>
   )
 }
