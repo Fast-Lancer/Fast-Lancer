c746529a80bd8530ba5ec23ff6517faff602b2e8
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2022-01-18 17:04:03 -0800

    Title and TitleNav components v1.0

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -1,29 +1,11 @@
-import { Link } from "react-router-dom";
 import styles from "./Title.module.css";
+import TitleNav from "./TitleNav";
 
 export default function Title({ pageTitle }) {
   return (
     <div className={styles.titleBody}>
       <section className={styles.title}>{pageTitle}</section>
-      <ul className={styles.tabnav}>
-        <li className={styles.active}>
-          <a href="#">{pageTitle}</a>
-        </li>
-        <li className={styles.tab2}>
-          {pageTitle === "Projects" ? (
-            <Link to="/clients">Clients</Link>
-          ) : (
-            "Projects"
-          )}
-        </li>
-        <li className={styles.tab3}>
-          {pageTitle === "Projects" ? (
-            <Link to="/newproject">New Project</Link>
-          ) : (
-            <Link to="/newclient">New Client</Link>
-          )}
-        </li>
-      </ul>
+      <TitleNav pageTitle={pageTitle} />
     </div>
   );
 }
---
+++ src/components/Title/TitleNav.jsx
@@ -0,0 +1,93 @@
+import styles from "./Title.module.css";
+import { Link } from "react-router-dom";
+
+export default function TitleNav({ pageTitle }) {
+  switch (pageTitle) {
+    case "projects":
+      return (
+        <ul className={styles.tabnav}>
+          <li className={styles.active}>
+            <a href={" "}>{pageTitle}</a>
+          </li>
+          <li>
+            <Link to="/clients">Clients</Link>
+          </li>
+          <li>
+            <Link to="/newproject">New Project</Link>
+          </li>
+        </ul>
+      );
+
+    case "clients":
+      return (
+        <ul className={styles.tabnav}>
+          <li className={styles.active}>
+            <a href={" "}>{pageTitle}</a>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link to="/newclient">New Client</Link>
+          </li>
+        </ul>
+      );
+
+    case "project detail":
+      return (
+        <ul className={styles.tabnav}>
+          <li className={styles.active}>
+            <a href={"/projects"}>Projects</a>
+          </li>
+          <li>
+            <Link to="/clients">Clients</Link>
+          </li>
+          <li>
+            <Link to="/editproject">Edit Project</Link>
+          </li>
+        </ul>
+      );
+
+    case "client detail":
+      return (
+        <ul className={styles.tabnav}>
+          <li className={styles.active}>
+            <a href={"/clients"}>Clients</a>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link to="/editclient">Edit Client</Link>
+          </li>
+        </ul>
+      );
+
+    case "new-edit project":
+      return (
+        <ul className={styles.tabnav}>
+          <li className={styles.active}>
+            <a href={"/projects"}>Projects</a>
+          </li>
+          <li>
+            <Link to="/clients">Clients</Link>
+          </li>
+        </ul>
+      );
+
+    case "new-edit client":
+      return (
+        <ul className={styles.tabnav}>
+          <li className={styles.active}>
+            <a href={"/clients"}>Clients</a>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+        </ul>
+      );
+
+    default:
+      return "err";
+  }
+}
