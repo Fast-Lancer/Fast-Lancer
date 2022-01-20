4b5efa082c99ec92478bae9d0d12951259161eaf
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2022-01-20 10:31:08 -0800

    testing defaultValue error

--- src/components/NewEditForm/NewEditForm.jsx
+++ src/components/NewEditForm/NewEditForm.jsx
@@ -12,7 +12,6 @@ export default function NewEditForm({ formSubmit, formLabel, isNew }) {
   const [notes, setNotes] = useState('')
   const [client, setClient] = useState('')
 
-
   useEffect(() => {
     if (!isNew){
       try {
@@ -22,58 +21,104 @@ export default function NewEditForm({ formSubmit, formLabel, isNew }) {
         isNew = true
       }}
   }, [])
-
+  console.log(client)
   const handleSubmit = async (e) => {
     e.preventDefault()
     await formSubmit({ client_name, email, phone, business_name, notes, user_id: user.id })
   }
-
-
   return <div>
     <h2>{formLabel}</h2>
     <fieldset>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor='client_name'>Client Name</label>
-        <input 
-          id='client_name'
-          type='text'
-          value={client_name}
-          required
-          onChange={(e) => setClient_name(e.target.value)} />
 
-        <label htmlFor='email'>E-Mail</label>
-        <input 
-          id='email'
-          type='email'
-          value={email}
-          required
-          onChange={(e) => setEmail(e.target.value)} />
+      { client ? 
+      // Edit Client Form
+        <form onSubmit={handleSubmit}>
+          <label htmlFor='client_name'>Client Name</label>
+          <input 
+            id='client_name'
+            type='text'
+            defaultValue={client.client_name}
+            required
+            onChange={(e) => setClient_name(e.target.value)} />
 
-        <label htmlFor='phone'>Phone Number</label>
-        <input 
-          id='phone'
-          type='number'
-          value={phone}
-          required
-          onChange={(e) => setPhone(e.target.value)} />
+          <label htmlFor='email'>E-Mail</label>
+          <input 
+            id='email'
+            type='email'
+            value={client.email}
+            required
+            onChange={(e) => setEmail(e.target.value)} />
 
-        <label htmlFor='business_name'>Business Name</label>
-        <input 
-          id='business_name'
-          type='text'
-          value={business_name}
-          placeholder='Optional'
-          onChange={(e) => setBusiness_name(e.target.value)} />
+          <label htmlFor='phone'>Phone Number</label>
+          <input 
+            id='phone'
+            type='text'
+            value={client.phone}
+            required
+            onChange={(e) => setPhone(e.target.value)} />
 
-        <label htmlFor='notes'>Notes</label>
-        <textarea
-          id='notes'
-          placeholder='Optional'
-          value={notes}
-          onChange={(e) => setNotes(e.target.value)}
-        />
-        <button type='submit'>{formLabel}</button>
-      </form>
+          <label htmlFor='business_name'>Business Name</label>
+          <input 
+            id='business_name'
+            type='text'
+            value={client.business_name}
+            placeholder='Optional'
+            onChange={(e) => setBusiness_name(e.target.value)} />
+
+          <label htmlFor='notes'>Notes</label>
+          <textarea
+            id='notes'
+            placeholder='Optional'
+            value={client.notes ? client.notes : undefined}
+            onChange={(e) => setNotes(e.target.value)}
+          />
+          <button type='submit'>{formLabel}</button>
+        </form>  
+        :
+        // New Client Form
+        <form onSubmit={handleSubmit}>
+          <label htmlFor='client_name'>Client Name</label>
+          <input 
+            id='client_name'
+            type='text'
+            value={client_name}
+            required
+            onChange={(e) => setClient_name(e.target.value)} />
+
+          <label htmlFor='email'>E-Mail</label>
+          <input 
+            id='email'
+            type='email'
+            value={email}
+            required
+            onChange={(e) => setEmail(e.target.value)} />
+
+          <label htmlFor='phone'>Phone Number</label>
+          <input 
+            id='phone'
+            type='number'
+            value={phone}
+            required
+            onChange={(e) => setPhone(e.target.value)} />
+
+          <label htmlFor='business_name'>Business Name</label>
+          <input 
+            id='business_name'
+            type='text'
+            value={business_name}
+            placeholder='Optional'
+            onChange={(e) => setBusiness_name(e.target.value)} />
+
+          <label htmlFor='notes'>Notes</label>
+          <textarea
+            id='notes'
+            placeholder='Optional'
+            value={notes}
+            onChange={(e) => setNotes(e.target.value)}
+          />
+          <button type='submit'>{formLabel}</button>
+        </form>
+      }
     </fieldset>
   </div>
 }
