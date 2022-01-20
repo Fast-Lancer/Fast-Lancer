ffcc3475ac3c7e9b1a6a6269eee15652c412bb81
Dylan Floyd <floyd800@gmail.com>
2022-01-19 17:20:30 -0800

    project styling

--- src/components/ProjectItem/ProjectItem.jsx
+++ src/components/ProjectItem/ProjectItem.jsx
@@ -3,15 +3,19 @@ import styles from './ProjectItem.module.css'
 export default function ProjectItem({ project }) {
   return (
     <div className={styles.projectItem}>
-      <section className={styles.titleSection}>
-        <h1>{project.title}</h1>
-        <h2 className={styles.clientName}>{project.clients.name}</h2>
-      </section>
-      <section>
+      <div className={styles.col}>
+        <h3 className={styles.h3}>
+          {project.title}
+        </h3>
+        <h4 className={styles.h4}>
+          {project.clients.client_name}
+        </h4>
+      </div>
+      <div className={styles.col}>
         <div>Hourly Rate: {project.hourly_rate}</div>
         <div>Hours Quoted: {project.hours_quoted}</div>
         <div>Quoted Price: {project.price_quoted}</div>
-      </section>
+      </div>
     </div>
   )
 }
