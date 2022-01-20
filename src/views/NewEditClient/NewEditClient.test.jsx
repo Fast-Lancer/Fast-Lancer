372d6dfb7abadbde700928834c87b88a8df906ce
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2022-01-20 14:13:44 -0800

    New and Edit Client views tested, passing

---
+++ src/views/NewEditClient/NewEditClient.test.jsx
@@ -0,0 +1,43 @@
+import { render, screen } from '@testing-library/react'
+import { Route, Switch } from 'react-router-dom'
+import { MemoryRouter } from 'react-router-dom'
+import NewEditClient from './NewEditClient.jsx'
+import { server } from '../../apiMocks/server.js'
+
+beforeAll(() => {
+  server.listen()
+})
+
+afterAll(() => {
+  server.close()
+})
+
+it('should render the New Client view', async () => {
+  const { container } = render(
+    <MemoryRouter initialEntries={['/']}>
+      <Switch>
+        <Route path='/'>
+          <NewEditClient isNew/>
+        </Route>
+      </Switch>
+    </MemoryRouter>
+  )
+
+  await screen.findByText('Create New Client')
+  expect(container).toMatchSnapshot()
+})
+
+it('should render the EDIT Client view', async () => {
+  const { container } = render(
+    <MemoryRouter initialEntries={['/']}>
+      <Switch>
+        <Route path='/'>
+          <NewEditClient/>
+        </Route>
+      </Switch>
+    </MemoryRouter>
+  )
+
+  await screen.findByText('Update Client')
+  expect(container).toMatchSnapshot()
+})
