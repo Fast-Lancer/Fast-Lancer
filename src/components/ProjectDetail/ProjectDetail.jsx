c631c1f047b5b6942452336393a4a165a68696b5
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2022-01-21 12:35:26 -0800

    proj detail styles

--- src/components/ProjectDetail/ProjectDetail.jsx
+++ src/components/ProjectDetail/ProjectDetail.jsx
@@ -1,4 +1,7 @@
 import styles from './ProjectDetail.module.css'
+import clock from '../../assets/icons/clockSmall.png'
+import www from '../../assets/icons/wwwSmall.png'
+import money from '../../assets/icons/moneySmall.png'
 
 export default function ProjectDetail({ project }) {
   const { hourly_rate, URL, date_start, date_end, description, notes, clients, hours_worked, price_quoted, hours_quoted } = project
@@ -6,32 +9,37 @@ export default function ProjectDetail({ project }) {
   return (
     <main>
       <div className={styles.projectDetail}>
+      Client Name
         <div className={styles.clientName}>
+         
           <h1 aria-label='client-name'>
             {clients?.client_name ?? 'No client name entered'}
           </h1>
         </div>
         <section className={styles.infoSection}>
-          <div aria-label='hourly-rate'>Hourly Rate: ${hourly_rate}</div>
-          {URL && <div aria-label='URL'>URL: {URL}</div>}
-          <div aria-label='start-date'>Start Date: {date_start}</div>
-          <div aria-label='end-date'>End Date: {date_end}</div>
+          <div className={styles.infoItem}aria-label='hourly-rate'>
+            <img src={money} alt='money'/>
+            Hourly Rate: ${hourly_rate}</div>
+
+          {URL && <div className={styles.infoItem} aria-label='URL'><img src={www} alt='www'/>URL: {URL}</div>}
+          <div className={styles.infoItem}aria-label='start-date'><img src={clock} alt='clock'/>Start Date: {date_start}</div>
+          <div className={styles.infoItem}aria-label='end-date'><img src={clock} alt='clock'/>Deadline: {date_end}</div>
         </section>
         <section className={styles.alignLeft}>
-          <h2>Description</h2>
-          <p aria-label='description'>
+          <h2>Project Description:</h2>
+          <p className={styles.textBlock}aria-label='description'>
             {description ? description : 'No description entered.'}
           </p>
         </section>
         <section className={styles.alignLeft}>
-          <h2>Hours and Pricing</h2>
-          <p>Hours Worked: {hours_worked}</p>
-          <p>Hours Quoted: {hours_quoted}</p>
-          <p>Price Quoted: {price_quoted}</p>
+          <h2>Hours and Pricing:</h2>
+          <p className={styles.alignItem}><img src={clock} alt='clock'/>Hours Worked: {hours_worked}</p>
+          <p className={styles.alignItem}><img src={clock} alt='clock'/>Hours Quoted: {hours_quoted}</p>
+          <p className={styles.alignItem}><img src={money} alt='money'/>Price Quoted: ${price_quoted}</p>
         </section>
         <section className={styles.alignLeft}>
           <h2>Notes</h2>
-          <p aria-label='notes'>
+          <p className={styles.textBlock} aria-label='notes'>
             {notes ? notes : 'No notes entered.'}
           </p>
         </section>
