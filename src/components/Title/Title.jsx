e25ae9340937252ed494931a718bcfa2418fb9b3
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2022-04-02 13:06:10 -0700

    adjusting project detail styles & headers

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -4,7 +4,6 @@ import TitleNav from './TitleNav'
 export default function Title({ pageTitle, pageHeader }) {
   return (
     <div className={styles.titleBody}>
-      <section className={styles.title}>{pageHeader}</section>
       <TitleNav pageTitle={pageTitle} pageHeader={pageHeader} />
     </div>
   )
